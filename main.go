5d77a932e3e623d46d00ba189b0dadc20153cdb9
George Brighton <george@gebn.co.uk>
2019-01-30 00:04:57 +0000

    Reduce line length of flags

--- main.go
+++ main.go
@@ -21,9 +21,15 @@ import (
 )
 
 var (
-	backupDir = kingpin.Flag("dir", "path of the autobackup directory").Default("/var/lib/unifi/backup/autobackup").ExistingDir()
-	bucket    = kingpin.Flag("bucket", "name of the S3 bucket to upload to").Required().String()
-	prefix    = kingpin.Flag("prefix", "prepended to the file name to form the object key of backups").Default("unifi/").String()
+	backupDir = kingpin.Flag("dir", "path of the autobackup directory").
+			Default("/var/lib/unifi/backup/autobackup").
+			ExistingDir()
+	bucket = kingpin.Flag("bucket", "name of the S3 bucket to upload to").
+		Required().
+		String()
+	prefix = kingpin.Flag("prefix", "prepended to the file name to form the object key of backups").
+		Default("unifi/").
+		String()
 )
 
 /*
