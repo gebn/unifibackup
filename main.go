23402d64920a30afbf8bfcdc8b67f1e9532919b6
George Brighton <george@gebn.co.uk>
2019-03-17 23:52:16 +0000

    Move S3 uploading into separate package

--- main.go
+++ main.go
@@ -5,15 +5,12 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"sync"
-	"time"
 
 	"github.com/gebn/unifibackup/monitor"
+	"github.com/gebn/unifibackup/uploader"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/gebn/go-stamp"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
@@ -30,75 +27,20 @@ var (
 		String()
 )
 
-/*
-uploadProcessor encapsulates the upload and delete logic. The returned
-function should be called with successive completed backup file paths, which
-will trigger upload followed by removal of the previous backup.
-*/
-func uploadProcessor(sess *session.Session) func(string) error {
-	svc := s3.New(sess)
-	uploader := s3manager.NewUploaderWithClient(svc)
-	var previous string
-	return func(path string) error {
-		f, err := os.Open(path)
-		if err != nil {
-			return fmt.Errorf("Failed to open %v for uploading: %v", path, err)
-		}
-
-		base := filepath.Base(path)
-		key := *prefix + base
-		start := time.Now()
-		_, err = uploader.Upload(&s3manager.UploadInput{
-			Bucket: bucket,
-			Key:    &key,
-			Body:   f,
-		})
-		elapsed := time.Since(start)
-		f.Close()
-		if err != nil {
-			return fmt.Errorf("Failed to upload %v: %v", base, err)
-		}
-		log.Printf("Uploaded %v in %v", base, elapsed.Round(time.Millisecond))
-
-		if previous != "" { // delete old backup *after* uploading new one
-			_, err := svc.DeleteObject(&s3.DeleteObjectInput{
-				Bucket: bucket,
-				Key:    &previous,
-			})
+func backupLoop(uploader *uploader.Uploader, monitor *monitor.Monitor, done <-chan struct{}) error {
+	for {
+		select {
+		case path := <-monitor.Backups:
+			_, err := uploader.Upload(path)
 			if err != nil {
-				// too many backups is a relatively benign failure, so
-				// continue as normal
-				log.Printf("Failed to delete %v: %v", previous, err)
+				return fmt.Errorf("upload error: %v", err)
 			}
+		case err := <-monitor.Errors:
+			return fmt.Errorf("monitor error: %v", err)
+		case <-done:
+			return nil
 		}
-		previous = key
-		return nil
 	}
-}
-
-/*
-upload sends each new backup to S3, deleting the previous one, if any.
-this function provides errors on the returned channel, but waits until done is closed before stopping
-*/
-func upload(sess *session.Session, backups <-chan string, done <-chan struct{}, wg *sync.WaitGroup) <-chan error {
-	errors := make(chan error)
-	process := uploadProcessor(sess)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for {
-			select {
-			case path := <-backups:
-				err := process(path)
-				if err != nil {
-					errors <- err
-				}
-			case <-done:
-				return
-			}
-		}
-	}()
-	return errors
 }
 
 func main() {
@@ -120,22 +62,14 @@ func main() {
 		log.Fatal(err)
 	}
 
-	var wg sync.WaitGroup
 	sess := session.Must(session.NewSession())
-	syncErrors := upload(sess, monitor.Backups, done, &wg)
-
-	select {
-	case err := <-monitor.Errors:
-		log.Println("Monitor error:", err)
-		close(done)
-	case err := <-syncErrors:
-		log.Println("Sync error:", err)
-		close(done)
-	case <-done:
+	svc := s3.New(sess)
+	uploader := uploader.New(svc, *bucket, *prefix)
+	if err = backupLoop(uploader, monitor, done); err != nil {
+		log.Println(err)
 	}
 
 	if err = monitor.Close(); err != nil {
 		log.Printf("Failed to close monitor: %v", err)
 	}
-	wg.Wait()
 }
---
+++ uploader/uploader.go
@@ -0,0 +1,64 @@
+package uploader
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3iface"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+)
+
+type Uploader struct {
+	Manager     *s3manager.Uploader
+	Bucket      string
+	Prefix      string
+	previousKey string
+}
+
+func New(svc s3iface.S3API, bucket, prefix string) *Uploader {
+	return &Uploader{
+		Manager: s3manager.NewUploaderWithClient(svc),
+		Bucket:  bucket,
+		Prefix:  prefix,
+	}
+}
+
+func (u *Uploader) Upload(path string) (*s3manager.UploadOutput, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open %v for uploading: %v", path, err)
+	}
+	defer f.Close()
+
+	base := filepath.Base(path)
+	key := u.Prefix + base
+	start := time.Now()
+	uploaded, err := u.Manager.Upload(&s3manager.UploadInput{
+		Bucket: &u.Bucket,
+		Key:    &key,
+		Body:   f,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("Failed to upload %v: %v", base, err)
+	}
+	elapsed := time.Since(start)
+	log.Printf("Uploaded %v in %v", base, elapsed.Round(time.Millisecond))
+
+	if u.previousKey != "" { // delete old backup *after* uploading new one
+		_, err := u.Manager.S3.DeleteObject(&s3.DeleteObjectInput{
+			Bucket: &u.Bucket,
+			Key:    &u.previousKey,
+		})
+		if err != nil {
+			// too many backups is a relatively benign failure, so
+			// continue as normal
+			log.Printf("Failed to delete %v: %v", u.previousKey, err)
+		}
+	}
+	u.previousKey = key
+	return uploaded, nil
+}
